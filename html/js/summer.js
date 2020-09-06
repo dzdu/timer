94ffcf324e8f231486a5483aa1c245fb21cffbc5
instdeadangel <513dragan@gmail.com>
2020-09-06 15:00:32 +0200

    First commit

---
+++ html/js/summer.js
@@ -0,0 +1,23 @@
+let countDate = new Date('Jun 1, 2021, 00:00')
+//let countDate = new Date('Sep 6, 2020, 14:15:30')
+
+let t = setInterval(() => {
+    let now = new Date().getTime();
+    let distance = countDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days + ' <br/> <span>days</span>'
+    document.getElementById('hours').innerHTML = hours + ' <br/> <span>hours</span>'
+    document.getElementById('mins').innerHTML = minutes + ' <br/> <span>minutes</span>'
+    document.getElementById('sec').innerHTML = seconds + ' <br/> <span>seconds</span>'
+    // If the count down is finished, write some tett
+    if (distance < 0) {
+        clearInterval(t)
+        document.getElementById("h1").innerHTML = "Happy Summer!";
+        document.getElementById('time').style.display = 'none'
+        document.getElementById('h2').style.display = 'none'
+    }
+}, 1000);
